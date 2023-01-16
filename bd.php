101d7b5bccc4748f29d22a0b0a04329f1609ac80
bop64921 <david_bop@hotmail.com>
2023-01-17 00:40:27 +0100

    Tercer Commit

--- bd.php
+++ bd.php
@@ -9,7 +9,7 @@ $password="";
 
 
 try{
-    $conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDator;",$usuario,$password);
+    $conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos",$usuario,$password);
 }catch (Exception $ex){
     echo $ex ->getMessage();
 }
